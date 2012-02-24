e52bcde36178fa1ff1695931b94c110b8ee35175
baltram-lielb@web.de <baltram-lielb@web.de@af58a2ae-3b00-eaee-5492-cb68207423a6>
2012-02-24 11:47:44 +0000

    Converter class mCXactWriter.

---
+++ trunk/mimicry/source/Mimicry/mi_xactwriter.h
@@ -0,0 +1,21 @@
+#ifndef MI_XACTWRITER_H_INCLUDED
+#define MI_XACTWRITER_H_INCLUDED
+
+class mCXactWriter
+{
+public:
+    struct SOptions :
+        public eSConverterOptions
+    {
+    public:
+        SOptions( void );
+    public:
+        mTArray< mCString > * m_pReplacedMeshesArray;
+        MIBool                m_bReplaceOnlyVertices;
+        mCIOStreamBinary *    m_pBaseXactStream;
+    };
+public:
+    static mEResult WriteXactFileData( mCScene const & a_sceneSource, mCIOStreamBinary & a_streamDest, SOptions a_Options );
+};
+
+#endif
