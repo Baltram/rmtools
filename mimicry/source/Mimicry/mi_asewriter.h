12ec4ae33c57fc3d2289e097c7e7f010cb4f657b
baltram-lielb@web.de <baltram-lielb@web.de@af58a2ae-3b00-eaee-5492-cb68207423a6>
2012-03-31 20:25:52 +0000

    Added standard parameters to some converter classes.

--- mimicry/source/Mimicry/mi_asewriter.h
+++ mimicry/source/Mimicry/mi_asewriter.h
@@ -13,7 +13,7 @@ public:
         MIBool m_bGothicAscFormat;
     };
 public:
-    static mEResult WriteAseFileData( mCScene const & a_sceneSource, mCIOStreamBinary & a_streamDest, SOptions a_Options );
+    static mEResult WriteAseFileData( mCScene const & a_sceneSource, mCIOStreamBinary & a_streamDest, SOptions a_Options = SOptions() );
 };
 
 #endif
