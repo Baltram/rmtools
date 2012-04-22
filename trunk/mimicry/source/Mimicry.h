f4f7da4344b13944aa939fab7753c69a923445c4
baltram-lielb@web.de <baltram-lielb@web.de@af58a2ae-3b00-eaee-5492-cb68207423a6>
2012-04-22 19:29:38 +0000

    Added mCBuffer and mCRefStream classes.

--- trunk/mimicry/source/Mimicry.h
+++ trunk/mimicry/source/Mimicry.h
@@ -4,6 +4,7 @@
 #include "Mimicry\mi_include_standard_base.h"
 #include "Mimicry\mi_include_core.h"
 #include "Mimicry\mi_include_string.h"
+#include "Mimicry\mi_include_buffer.h"
 #include "Mimicry\mi_include_array.h"
 #include "Mimicry\mi_include_map.h"
 #include "Mimicry\mi_include_variant.h"
---
+++ trunk/mimicry/source/Mimicry/mi_buffer.h
@@ -0,0 +1,31 @@
+#ifndef MI_BUFFER_H_INCLUDED
+#define MI_BUFFER_H_INCLUDED
+
+class mCBuffer
+{
+public:
+    mCBuffer( MILPCVoid a_pSource, MIUInt a_uSize );
+    mCBuffer( MIUInt a_uSize, MIByte a_byteValue = 0 );
+    mCBuffer( mCBuffer const & a_bufSource );
+    mCBuffer( void );
+   ~mCBuffer( void );
+public:
+    mCBuffer &     operator =  ( mCBuffer const & a_bufSource );
+    MIByte const & operator [] ( MIUInt a_uOffset ) const;
+    MIByte &       operator [] ( MIUInt a_uOffset );
+    MIBool         operator == ( mCBuffer const & a_bufOther ) const;
+    MIBool         operator != ( mCBuffer const & a_bufOther ) const;
+public:
+    MIByte *       AccessMemory( void );
+    MIByte const * GetMemory( void ) const;
+    MIUInt         GetSize( void ) const;
+    void           Resize( MIUInt a_uNewSize );
+    void           SetAt( MIUInt a_uOffset, MILPVoid a_pSource, MIUInt a_uSize );
+    void           SetAt( MIUInt a_uOffset, MIByte a_byteValue, MIUInt a_uSize );
+    void           Swap( mCBuffer & a_bufOther );
+protected:
+    MIByte * m_pData;
+    MIUInt   m_uSize;
+};
+
+#endif
