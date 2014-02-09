dc2f083161df0ba3a536fedddb1d577aa6715d43
baltram-lielb@web.de <baltram-lielb@web.de@af58a2ae-3b00-eaee-5492-cb68207423a6>
2014-02-09 16:03:57 +0000

    Completed mCCooking.

--- trunk/mimicry/source/Mimicry/mi_cooking.h
+++ trunk/mimicry/source/Mimicry/mi_cooking.h
@@ -1,15 +1,22 @@
 #ifndef MI_COOKING_H_INCLUDED
 #define MI_COOKING_H_INCLUDED
 
+MI_ERROR_DEFINE_DERIVED( mCPhysXError,
+                         mCError,
+                         EMissingSystemSoftware,
+                         EMissingLegacySystemSoftware )
+
 class mCCooking
 {
 public:
-    static MIBool InitPhysicsSDK( MIUInt a_uPhysicsSDKVersion );
-    static MIBool ReadCookedMesh( mCIOStreamBinary & a_streamSource, mCMesh & a_meshDest );
-    static void   RegisterPhysXLoaderFunctions( MILPCVoid a_pCreatePhysicsSDK, MILPCVoid a_pReleasePhysicsSDK, MILPCVoid a_pGetCookingLib );
-    static void   RegisterMultiVersionFunctions( void ( * a_pfuncUnloadModule )( MILPCChar ), void ( * a_pfuncRequestPhysXLoaderFunction )( void ) );
-    static void   ReleaseSDK( void );
-    static MIBool WriteCookedMesh( mCIOStreamBinary & a_streamDest, mCMesh const & a_meshSource );
+    static MIBool   InitCooking( MIUInt a_uPhysicsSDKVersion );
+    static mEResult InitGothic3Cooking( void );
+    static mEResult InitRisenCooking( void );
+    static MIBool   ReadCookedMesh( mCIOStreamBinary & a_streamSource, mCMesh & a_meshDest );
+    static void     RegisterPhysXLoaderFunctions( MILPCVoid a_pCreatePhysicsSDK, MILPCVoid a_pReleasePhysicsSDK, MILPCVoid a_pGetCookingLib );
+    static void     RegisterMultiVersionFunctions( void ( * a_pfuncUnloadModule )( MILPCChar ), void ( * a_pfuncRequestPhysXLoaderFunction )( void ) );
+    static void     ReleaseSDK( void );
+    static MIBool   WriteCookedMesh( mCIOStreamBinary & a_streamDest, mCMesh a_meshSource, MIBool a_bConvex = MIFalse );
 };
 
 #endif
